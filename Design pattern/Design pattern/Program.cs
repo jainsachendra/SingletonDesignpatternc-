8dd20e3cc593acfa8b5b7caddc9e7093ec0e049f
jainsachendra <jainsachendra98@gmail.com>
2020-05-02 02:04:21 +0530

    Add files via upload

---
+++ Design pattern/Design pattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                ()=>NewMethod(),
+                ()=>NewMethod1()
+                ) ;
+
+         
+            
+            Console.ReadLine();
+        }
+
+        private static void NewMethod1()
+        {
+            Signleton s2 = Signleton.getinstance;
+            s2.printmessage("secondobj");
+        }
+
+        private static void NewMethod()
+        {
+            Signleton s1 = Signleton.getinstance;
+            s1.printmessage("firstobj");
+        }
+    }
+}
---
+++ Design pattern/Design pattern/Signleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattern
+{
+   public sealed class Signleton
+    {
+        private static int counter = 0;
+        private static Signleton instance = null;
+        private static readonly object obj = new object();
+        public static Signleton getinstance
+        {
+            get
+            {
+                lock (obj)
+                {
+                       if (instance == null)
+                    {
+                        instance = new Signleton();
+                    }
+                }
+                return instance;
+                
+            }
+        }
+        private Signleton()
+        {
+            counter++;
+            Console.WriteLine("conter" + counter.ToString() );
+
+        }
+        public void printmessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
